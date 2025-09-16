2be04a1e08c3bcedc02066550ea2f4f7cf1cb850
mariana-silva-ifpar <silva.mariana1@escolar.ifrn.edu.br>
2025-09-16 15:50:27 -0300

    feat: criação do projeto e das rotas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
+import { Busca } from './busca/busca';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'inicio',
+        component: Inicio
+    },
+    {
+        path: 'busca',
+        component: Busca
+    }
+];
